8fba197268851410272661b64fb38ad333306521
Dodge <20012011ryu@gmail.com>
2021-11-30 08:59:35 +0900

    first commit
    c# api and parser included

---
+++ BusWpf/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using BusWpf.API;
+using BusWpf.Model;
+
+namespace BusWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            APIClass apiClass = new APIClass();
+            
+            apiClass.BusCsv();
+            List<int> stationIndex = apiClass.FindStationNumber("홍대입구역");
+
+            //apiClass.FindStationInfoByIndex(stationIndex[0]);
+            for (int i = 0; i < stationIndex.Count; i++)
+            {
+                apiClass.FindStationInfoByIndex(stationIndex[i]);
+            }
+        }
+    }
+}
