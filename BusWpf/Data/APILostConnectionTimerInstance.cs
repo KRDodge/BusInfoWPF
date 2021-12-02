15fbb1cbeb939ad1c15045e388fff33c412a0a12
Dodge <20012011ryu@gmail.com>
2021-12-02 17:23:48 +0900

    timer 추가

---
+++ BusWpf/Data/APILostConnectionTimerInstance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace BusWpf.Data
+{
+    internal class APILostConnectionTimerInstance
+    {
+        private static APILostConnectionTimerInstance apiTimerInstance;
+
+        public event EventHandler LostConnectionTimerDone;
+        private DispatcherTimer lostConnectionTimer;
+
+        public void StartLostConnectionTimer()
+        {
+            lostConnectionTimer.Stop();
+            lostConnectionTimer.Interval = TimeSpan.FromSeconds(60);
+            lostConnectionTimer.Tick += new EventHandler(OnLostConnectionTimer_Tick);
+            lostConnectionTimer.Start();
+        }
+
+        private void OnLostConnectionTimer_Tick(object sender, EventArgs e)
+        {
+            if (LostConnectionTimerDone != null)
+                LostConnectionTimerDone(this, EventArgs.Empty);
+        }
+
+        public APILostConnectionTimerInstance()
+        {
+            initialize();
+        }
+
+        private void initialize()
+        {
+            lostConnectionTimer = new DispatcherTimer();
+        }
+
+        public static APILostConnectionTimerInstance GetInstance()
+        {
+            if (apiTimerInstance == null)
+                apiTimerInstance = new APILostConnectionTimerInstance();
+
+            return apiTimerInstance;
+        }
+    }
+}
---
+++ BusWpf/Data/APIPollingTimerInstance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace BusWpf.Data
+{
+    internal class APIPollingTimerInstance
+    {
+        private static APIPollingTimerInstance apiTimerInstance;
+        public event EventHandler PollingTimerDone;
+
+        private DispatcherTimer pollingTimer;
+
+        public APIPollingTimerInstance()
+        {
+            initialize();
+        }
+
+        private void initialize()
+        {
+            pollingTimer = new DispatcherTimer();
+        }
+
+        public static APIPollingTimerInstance GetInstance()
+        {
+            if (apiTimerInstance == null)
+                apiTimerInstance = new APIPollingTimerInstance();
+
+            return apiTimerInstance;
+        }
+
+        public void SetPollingTimer(int _time)
+        {
+            pollingTimer.Stop();
+            pollingTimer.Interval = TimeSpan.FromSeconds(_time);
+            pollingTimer.Tick += new EventHandler(OnPollingTimer_Tick);
+            pollingTimer.Start();
+        }
+
+        private void OnPollingTimer_Tick(object sender, EventArgs e)
+        {
+            if (PollingTimerDone != null)
+                PollingTimerDone(this, EventArgs.Empty);
+        }
+    }
+}
